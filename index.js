29eb7708b819db785a58d82e93d34823acddbe7e
Anwishta <anwishtaghosh@gmail.com>
2024-12-13 18:17:53 +0530

    Blog model created

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import connectDB from './Utils/db.js';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
+const app = express();
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
 
 //importing routea
 import userRoutes from './Routes/userRoutes.js';
-
-const app = express();
+import blogRoutes from './Routes/blogRoutes.js';
 
 const PORT = process.env.PORT || 8000;
 
@@ -24,3 +29,4 @@ connectDB()
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use('/api/blogs', blogRoutes);
