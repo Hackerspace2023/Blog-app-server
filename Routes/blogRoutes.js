372e8fb99695e5ea2142d01ccbc0a2746ca52b73
Anwishta <anwishtaghosh@gmail.com>
2024-12-14 19:29:14 +0530

    search-blog

--- Routes/blogRoutes.js
+++ Routes/blogRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPost, addComment, toggleLike, toggleBookmark } from "../Controllers/blogController.js";
+import { createPost, addComment, toggleLike, toggleBookmark, searchBlogsByTitle } from "../Controllers/blogController.js";
 import { authenticate } from "../Middlewares/authMiddleware.js"; // Middleware to protect routes
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.post("/", authenticate, createPost);
 router.post("/:id/comments", authenticate, addComment);
 router.post("/:postId/like", authenticate, toggleLike);
 router.post("/:postId/bookmark", authenticate, toggleBookmark);
+router.get("/search", authenticate, searchBlogsByTitle);
 
 export default router;
